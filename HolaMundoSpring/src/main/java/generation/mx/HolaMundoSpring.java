02a875a2cb894619d1a0b0048a6170b28cb22f7d
Yann <metalyanni@gmail.com>
2021-07-23 10:28:15 -0500

    Recibiendo parámetros mediante la URL y desplegando en mensaje

--- HolaMundoSpring/src/main/java/generation/mx/HolaMundoSpring.java
+++ HolaMundoSpring/src/main/java/generation/mx/HolaMundoSpring.java
@@ -2,6 +2,7 @@ package generation.mx;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //Con REST controller le decmos a java que esa aplicación intereactua con el servidor.
@@ -11,9 +12,11 @@ public class HolaMundoSpring {
 	/*DEntro del request mapping debemos especificar unas cosas, por ejemplo el Path, que es la ruta donde se ejecutará
 	Y especificar el método mediante el cual se lee.
 	Cuando entremos a esa ruta mediante el método GET.
+	El RequestParam lee los valores que recibe mediante la UDL
 	*/
 	@RequestMapping (path = "/", method = RequestMethod.GET)
-	public String holaMundo() {
-		return "";
+	public String holaMundo(@RequestParam(name = "nombre", defaultValue = "defecto") String nombre) {
+		return "Hola" + nombre;
 	}
+	//En URL mandamos el parametro y sería de la siguiente manera: localhost:808?nombre=Yann
 }
