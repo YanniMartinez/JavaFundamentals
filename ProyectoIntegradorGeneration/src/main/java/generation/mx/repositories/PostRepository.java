cd4c68c6ab5c76862da6ca2dcff598ef9c9be3c9
Yann <metalyanni@gmail.com>
2021-07-29 13:21:07 -0500

    Agregando un poco de comentarios

--- ProyectoIntegradorGeneration/src/main/java/generation/mx/repositories/PostRepository.java
+++ ProyectoIntegradorGeneration/src/main/java/generation/mx/repositories/PostRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import generation.mx.models.PostModel;
-
+/*JPA = Java Persistence API*/
 @Repository
 public interface PostRepository extends CrudRepository<PostModel, Long>{
 	/*
 	 * Buscando elementos por titulo cuando contengan cierta cadena
-	 * findBy nombreDelCampo Containing*/
+	 * findBy nombreDelCampo Containing
+	 * Containing significa que contengan eso en la busqueda*/
 	public abstract ArrayList<PostModel> findByTitleContaining(String title);
 	
 	/*
@@ -22,3 +23,8 @@ public interface PostRepository extends CrudRepository<PostModel, Long>{
 	 * */
 	public abstract ArrayList<PostModel> findByTitleContainingOrderByIdDesc(String title);
 }
+
+/*ORDEN DE MANEJO:
+ * 
+ * Repositorio --> Serivio --> Controlador
+ * */
